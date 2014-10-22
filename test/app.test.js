32d881e2d8d363fde24e8069d0042889bd2ad9d6
Rudi Giesler <rudi@praekelt.com>
2014-10-22 15:52:34 +0200

    Changed tests to reflect new state structure

--- test/app.test.js
+++ test/app.test.js
@@ -578,7 +578,7 @@ describe("App", function() {
                 .setup.user.addr('+12345')
                 .inputs('MN34', '1')
                 .check.interaction({
-                    state: 'states:send-report',
+                    state: 'states:notify-success',
                     reply: ['Thank you. We will forward your report to the ',
                             'City of Cape Town and let you know if there is ',
                             'an update.'
@@ -817,7 +817,7 @@ describe("App", function() {
                 .setup.user.addr('+12345')
                 .inputs("MN34", "6", "Custom issue")
                 .check.interaction({
-                    state: 'states:send-report',
+                    state: 'states:notify-success',
                     reply: ['Thank you. We will forward your report to the ',
                             'City of Cape Town and let you know if there is ',
                             'an update.'
