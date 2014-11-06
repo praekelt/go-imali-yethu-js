aef28ee2ea4a8940c1cdcf62cd312214a3080acb
Rudi Giesler <rudi@praekelt.com>
2014-11-06 11:28:39 +0200

    Change fixtures to represent timezone change

--- test/ona.fixtures.js
+++ test/ona.fixtures.js
@@ -12,17 +12,17 @@ onafixtures.submit.add({
             "issue":"broken_toilet",
             "fault_status":"logged",
             "toilet_location":"-34.01667 -18.66404",
-            "logged_date":"1970-01-01T00:00:01.337Z"
+            "logged_date":"1970-01-01T02:00:01+02:00"
         }
     },
     response: {
         data: {
             "instanceID": "uuid:4a89f4f7cc044e45a5f887487406307e",
             "encrypted": false,
-            "submissionDate": "1970-01-01T00:00:01.337Z",
+            "submissionDate": "1970-01-01T02:00:01+02:00",
             "formid": "1",
             "message": "Successful submission.",
-            "markedAsCompleteDate": "1970-01-01T00:00:01.337Z"
+            "markedAsCompleteDate": "1970-01-01T02:00:01+02:00"
         }
     }
 });
@@ -37,17 +37,17 @@ onafixtures.submit.add({
             "issue": "Custom issue",
             "fault_status":"logged",
             "toilet_location":"-34.01667 -18.66404",
-            "logged_date":"1970-01-01T00:00:01.337Z"
+            "logged_date":"1970-01-01T02:00:01+02:00"
         }
     },
     response: {
         data: {
             "instanceID": "uuid:4a89f4f7cc044e45a5f887487406307e",
             "encrypted": false,
-            "submissionDate": "1970-01-01T00:00:01.337Z",
+            "submissionDate": "1970-01-01T02:00:01+02:00",
             "formid": "1",
             "message": "Successful submission.",
-            "markedAsCompleteDate": "1970-01-01T00:00:01.337Z"
+            "markedAsCompleteDate": "1970-01-01T02:00:01+02:00"
         }
     }
 });
@@ -62,7 +62,7 @@ onafixtures.submit.add({
             "issue": "Error issue",
             "fault_status":"logged",
             "toilet_location":"-34.01667 -18.66404",
-            "logged_date":"1970-01-01T00:00:01.337Z"
+            "logged_date":"1970-01-01T02:00:01+02:00"
         }
     },
     response: {
@@ -82,17 +82,17 @@ onafixtures.submit.add({
             "toilet_cluster": "None",
             "issue": "broken_toilet",
             "fault_status": "logged",
-            "logged_date": "1970-01-01T00:00:01.337Z"
+            "logged_date": "1970-01-01T02:00:01+02:00"
         }
     },
     response: {
         data: {
             "instanceID": "uuid:4a89f4f7cc044e45a5f887487406307e",
             "encrypted": false,
-            "submissionDate": "1970-01-01T00:00:01.337Z",
+            "submissionDate": "1970-01-01T02:00:01+02:00",
             "formid": "1",
             "message": "Successful submission.",
-            "markedAsCompleteDate": "1970-01-01T00:00:01.337Z"
+            "markedAsCompleteDate": "1970-01-01T02:00:01+02:00"
         }
     }
 });
