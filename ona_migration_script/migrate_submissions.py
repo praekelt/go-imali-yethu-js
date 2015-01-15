7a3a9965df5a60f85a888e5f90ef2aa535760a2f
Rudi Giesler <rudi@praekelt.com>
2015-01-15 12:52:05 +0200

    Changed from_form and to_form to get from command line arguments

--- ona_migration_script/migrate_submissions.py
+++ ona_migration_script/migrate_submissions.py
@@ -1,12 +1,20 @@
+import argparse
+
 from ona import OnaApiClient
 import settings
-
-FROM_FORM = '15756'  # imaliyethu
-TO_FORM = '23666'  # per report
 
 DEFAULTS = {'msisdn': '', 'fault_status': '', 'logged_date': ''}
 
 client = OnaApiClient(settings.USERNAME, settings.PASSWORD)
+
+parser = argparse.ArgumentParser(description='Migrate submissions')
+parser.add_argument(
+    'from_id', type=str,
+    help="The id (number) of the form to migrate submissions from")
+parser.add_argument(
+    'to_id', type=str,
+    help="The id (number) of the form to migrate submissions to")
+args = parser.parse_args()
 
 
 def get_fields_from_form(formid):
@@ -16,10 +24,10 @@ def get_fields_from_form(formid):
         fields.append(child.get('name'))
     return fields
 
-from_fields = get_fields_from_form(FROM_FORM)
-to_fields = get_fields_from_form(TO_FORM)
+from_fields = get_fields_from_form(args.from_id)
+to_fields = get_fields_from_form(args.to_id)
 
-for submission in client.data_for_form(FROM_FORM):
+for submission in client.data_for_form(args.from_id):
     new_submission = submission.copy()
     # Remove deleted fields
     for field in submission:
@@ -33,7 +41,7 @@ for submission in client.data_for_form(FROM_FORM):
             else:
                 new_submission[field] = None
     # Post submission to new form
-    form_id_string = client.get_form(TO_FORM)['id_string']
+    form_id_string = client.get_form(args.to_id)['id_string']
     try:
         client.submission({
             "id": form_id_string,
