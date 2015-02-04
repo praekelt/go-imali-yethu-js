dda78db209fd0ace97f16d4fced7a44d670cb104
Rudi Giesler <rudi@praekelt.com>
2015-02-04 13:22:34 +0200

    Always print changes, not just for dry run

--- ona_migration_script/migrate_toilet_codes.py
+++ ona_migration_script/migrate_toilet_codes.py
@@ -53,7 +53,6 @@ if __name__ == '__main__':
     source_regex = re.compile(args.source_regex)
     for toilet in get_all_toilets(session, args.url):
         new_code = get_new_code(toilet, source_regex, args.target_regex)
-        if args.dryrun:
-            print '%s %s' % (toilet['code'], new_code)
-        else:
+        if not args.dryrun:
             change_toilet_code(session, args.url, toilet, new_code)
+        print '%s %s' % (toilet['code'], new_code)
