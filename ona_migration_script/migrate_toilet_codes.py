9f259f1c49f3fa406fcfb0608e37dea695d71fcb
Rudi Giesler <rudi@praekelt.com>
2015-02-04 10:40:30 +0200

    Add get_all_toilets function.

--- ona_migration_script/migrate_toilet_codes.py
+++ ona_migration_script/migrate_toilet_codes.py
@@ -1,9 +1,10 @@
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description='Migrate toilet codes')
 parser.add_argument(
     'url', type=str,
-    help='The base URL for the django toilet database')
+    help='The base URL for the django toilet code database')
 parser.add_argument(
     'username', type=str, help='The username used to log in')
 parser.add_argument(
@@ -11,4 +12,23 @@ parser.add_argument(
 parser.add_argument(
     '--dryrun', '-d', action='store_true',
     help='Print out changes instead of uploading them.')
-args = parser.parse_args()
+
+
+def create_session(user, password):
+    s = requests.Session()
+    s.auth = (user, password)
+    s.headers.update({"Content-type": "application/json; charset=utf-8"})
+    return s
+
+
+def get_all_toilets(session, url):
+    r = session.get(url)
+    r.raise_for_status()
+    return r.json()
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    session = create_session(args.username, args.password)
+    toilets = get_all_toilets(session, args.url)
+    print len(toilets)
