c8acf1ec3250ac1240ad78148ac0535e6369d9b1
Rudi Giesler <rudi@praekelt.com>
2015-02-04 11:55:31 +0200

    Added functions for changing toilet code with regex

--- ona_migration_script/migrate_toilet_codes.py
+++ ona_migration_script/migrate_toilet_codes.py
@@ -1,5 +1,8 @@
 import argparse
+import json
+import re
 import requests
+import urlparse
 
 parser = argparse.ArgumentParser(description='Migrate toilet codes')
 parser.add_argument(
@@ -9,6 +12,10 @@ parser.add_argument(
     'username', type=str, help='The username used to log in')
 parser.add_argument(
     'password', type=str, help='The password used to log in')
+parser.add_argument(
+    'source_regex', type=str, help='The regex that is applied to the code')
+parser.add_argument(
+    'target_regex', type=str, help='The regex construction of the new code')
 parser.add_argument(
     '--dryrun', '-d', action='store_true',
     help='Print out changes instead of uploading them.')
@@ -27,8 +34,26 @@ def get_all_toilets(session, url):
     return r.json()
 
 
+def get_new_code(toilet, source_regex, target_regex):
+    return source_regex.sub(target_regex, toilet['code'])
+
+
+def change_toilet_code(session, url, toilet, new_code):
+    data = {'code': new_code}
+    url = urlparse.urljoin(url, str(toilet['id']))
+    r = session.patch(url, json.dumps(data))
+    r.raise_for_status()
+    assert r.json()['code'] == new_code
+    return r
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     session = create_session(args.username, args.password)
-    toilets = get_all_toilets(session, args.url)
-    print len(toilets)
+    source_regex = re.compile(args.source_regex)
+    for toilet in get_all_toilets(session, args.url):
+        new_code = get_new_code(toilet, source_regex, args.target_regex)
+        if args.dryrun:
+            print '%s %s' % (toilet['code'], new_code)
+        else:
+            change_toilet_code(session, args.url, toilet, new_code)
